aaca38da7743b8f656e06d42042be847915785a9
Dave Strock <dave.strock@gmail.com>
2012-01-12 14:45:02 -0600

    Enabled rinari, magit, and yasnippet.

--- emacs.d/d1/modes.el
+++ emacs.d/d1/modes.el
@@ -1,11 +1,20 @@
 ;; load other, mode detailed configs
 (load "d1/shell")
-;; (load "d1/ruby")
 
-;; textmate
 (vendor 'textmate)
 (textmate-mode)
 ;;(setq textmate-find-files-command "git ls-tree --full-tree --name-only -r HEAD")
+
+(vendor 'rinari)
+(require 'rinari)
+
+(vendor 'magit)
+(require 'magit)
+
+(vendor 'yasnippet)
+(require 'yasnippet)
+(setq yas/snippet-dirs '("~/.emacs.d/vendor/yasnippet/snippets" "~/.emacs.d/d1/snippets"))
+(yas/global-mode 1)
 
 ;; load ecb
 ;;(add-to-list 'load-path "~/.dotfiles/emacs.d/vendor/emacsmirror-ecb-54527e9")
