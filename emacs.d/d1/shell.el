60b8edef18f0c4d4bbff8f4bf36388bf47b6e1ee
Dave Strock <dave.strock@gmail.com>
2012-01-24 13:13:12 -0600

    Changed comint mode so only up and down cycle through shell commands. This leaves C-p and C-n for moving around the buffers!

--- emacs.d/d1/shell.el
+++ emacs.d/d1/shell.el
@@ -4,6 +4,5 @@
 (eval-after-load 'shell
   '(progn
      (define-key shell-mode-map [up] 'comint-previous-input)
-     (define-key shell-mode-map [down] 'comint-next-input)
-     (define-key shell-mode-map "\C-p" 'comint-previous-input)
-     (define-key shell-mode-map "\C-n" 'comint-next-input)))
+     (define-key shell-mode-map [down] 'comint-next-input)))
+
