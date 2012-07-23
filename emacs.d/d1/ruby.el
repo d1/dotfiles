05158441e0c313015d37cd66540b25bbe1c2ad45
Dave Strock <dave.strock@gmail.com>
2012-07-23 14:12:01 -0500

    Adds emacs-pry

--- emacs.d/d1/ruby.el
+++ emacs.d/d1/ruby.el
@@ -6,3 +6,8 @@
 (require 'rvm)
 (add-hook 'ruby-mode-hook
           (lambda () (rvm-activate-corresponding-ruby)))
+
+(vendor 'pry)
+(require 'pry)
+(global-set-key [S-f9] 'pry-intercept)
+(global-set-key [f9] 'pry-intercept-rerun)
