e2039f4eba9bd3bf217b2447abc29bb024bec884
Dave Strock <dave.strock@gmail.com>
2012-07-23 14:28:04 -0500

    Adds emacs cucumber support (feature-mode)

--- emacs.d/d1/modes.el
+++ emacs.d/d1/modes.el
@@ -14,6 +14,11 @@
 (setq yas/snippet-dirs '("~/.emacs.d/vendor/yasnippet/snippets" "~/.emacs.d/d1/snippets"))
 (yas/global-mode 1)
 
+;; Cucumber
+(vendor 'feature-mode)
+(require 'feature-mode)
+(add-to-list 'auto-mode-alist '("\.feature$" . feature-mode))
+
 ;; load ecb
 ;;(add-to-list 'load-path "~/.dotfiles/emacs.d/vendor/emacsmirror-ecb-54527e9")
 ;;(require 'ecb)
