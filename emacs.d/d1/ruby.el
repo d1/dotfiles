3845fdbb922c54d49118a8d09b67b8a73db7518a
Dave Strock <dave.strock@gmail.com>
2012-07-23 14:20:19 -0500

    Adds emacs rspec-mode

--- emacs.d/d1/ruby.el
+++ emacs.d/d1/ruby.el
@@ -7,6 +7,9 @@
 (add-hook 'ruby-mode-hook
           (lambda () (rvm-activate-corresponding-ruby)))
 
+(vendor 'rspec-mode)
+(require 'rspec-mode)
+
 (vendor 'pry)
 (require 'pry)
 (global-set-key [S-f9] 'pry-intercept)
