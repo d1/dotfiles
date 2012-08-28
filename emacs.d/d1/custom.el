da4f07285fd9841afaf31ded672d4f438ca7b89b
Dave Strock <dave.strock@gmail.com>
2012-08-28 14:26:01 -0500

    Reconfigure hippy expand to be a bit closer to dabbrev

--- emacs.d/d1/custom.el
+++ emacs.d/d1/custom.el
@@ -3,6 +3,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(hippie-expand-try-functions-list (quote (try-expand-dabbrev try-expand-dabbrev-all-buffers try-expand-dabbrev-from-kill try-complete-file-name-partially try-complete-file-name try-expand-all-abbrevs try-expand-list try-expand-line try-complete-lisp-symbol-partially try-complete-lisp-symbol))))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
